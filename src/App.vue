6f4421cba9c27612ec4e8eea5395a4a560c90f25
henkyaliazki <henkyaliazkipratama>
2022-12-30 19:43:34 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="container mt-5">
+    <h1>AZShop</h1>
+    <div>
+      <p class="animate__animated animate__fadeInDown">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, enim vero reiciendis quibusdam rem voluptatem inventore nobis aliquam commodi error facilis cumque ut eveniet asperiores. Voluptatum modi veritatis deserunt
+        harum.
+      </p>
+      <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "App",
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
